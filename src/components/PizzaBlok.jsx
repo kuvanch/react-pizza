6ab8f554f4ef88579244ac61f1c94a60c494cb2a
kuvanch <kolyabek13@gmail.com>
2021-02-28 16:32:38 +0500

    Реализовал PizzaBlock

--- src/components/PizzaBlok.jsx
+++ src/components/PizzaBlok.jsx
@@ -1,23 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
+import classNames from 'classnames'
+function PizzaBlok({name, imageUrl, price, types,sizes}) {
+  const typesNames = ['тонкое','традиционное']
+  const avilableSizes = [26,30,45]
+  const [activeType, setActiveType] = useState(types[0])
+  const [activeSize, setActiveSize] = useState(sizes[0])
 
-function PizzaBlok({name, imageUrl}) {
+
+  const onSelectType = (index) => {
+    setActiveType(index)
+  }
+  const onSelectSize = (index) => {
+    setActiveSize(index)
+  }
     return (
         <div className="pizza-block">
             <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
             <h4 className="pizza-block__title">{name}</h4>
             <div className="pizza-block__selector">
               <ul>
-                <li className="active">тонкое</li>
-                <li>традиционное</li>
+                {
+                  typesNames.map((type,idx) => {
+                   return <li 
+                      key={type}
+                      onClick={() => onSelectType(idx)}
+                      className={classNames({
+                        active: activeType === idx,
+                        disabled: !types.includes(idx)
+                      })}>{type}</li>
+                  })
+                }
               </ul>
               <ul>
-                <li className="active">26 см.</li>
-                <li>30 см.</li>
-                <li>40 см.</li>
+              {
+                  avilableSizes.map((size,idx) => {
+                   return <li 
+                      key={size}
+                      onClick={() => onSelectSize(idx)}
+                      className={classNames({
+                        active: activeSize === idx,
+                        disabled: !sizes.includes(size)
+                      })}>{size} см.</li>
+                  })
+                }
               </ul>
             </div>
             <div className="pizza-block__bottom">
-              <div className="pizza-block__price">от 395 ₽</div>
+              <div className="pizza-block__price">от {price} ₽</div>
               <div className="button button--outline button--add">
                 <svg width={12} height={12} viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z" fill="white" />
